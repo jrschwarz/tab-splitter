bc536cbd7cdb99f7c5c592f8b6776059803795b9
Joseph Schwarz <joseph.schwarz@ibm.com>
2021-03-29 23:36:57 -0400

    Use grouped tableview

--- Tab Splitter/Tab Splitter/TableInputs/TableInputsView.swift
+++ Tab Splitter/Tab Splitter/TableInputs/TableInputsView.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class TableInputsView: NSObject, UITableViewDelegate, UITableViewDataSource {
     
-    var superView: UIView?
     var tableView: UITableView
-    var heightConstraint: NSLayoutConstraint
     let tableRowHeight: CGFloat = 43
+    
+    enum sections: Int {
+        case name
+        case people
+    }
     
     var items: [String?] = [nil]
     
@@ -22,12 +25,8 @@ class TableInputsView: NSObject, UITableViewDelegate, UITableViewDataSource {
     
     var addLabel: String = "Add"
     
-    init(superView: UIView?, tableView: UITableView) {
-        self.superView = superView
+    init(tableView: UITableView) {
         self.tableView = tableView
-        
-        let initialHeight = CGFloat(items.count) * tableRowHeight
-        heightConstraint = tableView.heightAnchor.constraint(equalToConstant: initialHeight)
         
         super.init()
         
@@ -35,11 +34,6 @@ class TableInputsView: NSObject, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         tableView.isEditing = true
         tableView.rowHeight = tableRowHeight
-        tableView.estimatedRowHeight = 0
-        tableView.estimatedSectionHeaderHeight = 0
-        tableView.estimatedSectionFooterHeight = 0
-        tableView.isScrollEnabled = false
-        heightConstraint.isActive = true
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,7 +52,6 @@ class TableInputsView: NSObject, UITableViewDelegate, UITableViewDataSource {
         if indexPath.row == addRowIndex {
             let cell = tableView.dequeueReusableCell(withIdentifier: "TableAddInputCell")!
             cell.textLabel?.text = addLabel
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: tableView.bounds.width)
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "TableInputCell") as! TableInputCell
@@ -69,29 +62,17 @@ class TableInputsView: NSObject, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            UIView.animate(withDuration: 0.23, delay: 0, options: .curveEaseOut) {
-                self.items.remove(at: indexPath.row)
-                self.tableView.deleteRows(at: [indexPath], with: .automatic)
-                self.updateHeight()
-            }
+            self.items.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
         }
         
         if editingStyle == .insert {
             let insertAt = items.count - 1
             let itemNum = items.count
-            UIView.animate(withDuration: 0.3) {
-                self.items.insert("Person \(itemNum)", at: insertAt)
-                self.updateHeight()
-                self.tableView.insertRows(at: [IndexPath(row: insertAt, section: 0)], with: .bottom)
-            }
+            self.items.insert("Person \(itemNum)", at: insertAt)
+            self.tableView.insertRows(at: [IndexPath(row: insertAt, section: 0)], with: .bottom)
             
         }
-    }
-    
-    func updateHeight() {
-        let newHeight = CGFloat(items.count) * tableView.rowHeight
-        self.heightConstraint.constant = newHeight
-        self.superView?.layoutIfNeeded()
-        
     }
 }
